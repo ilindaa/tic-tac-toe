8bcb0e37923d40eaff12b81f607bdea7b653baa3
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-07-24 23:20:34 -0400

    Working on JS code

--- javascript.js
+++ javascript.js
@@ -1,21 +1,36 @@
-const gameBoard = (() => {
-    let gameboard = [];
-    return {
-        gameboard,
-    };
-})();
+const Gameboard = (() => {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-const displayController = (() => {
-    let displayBox = document.querySelector('.display-box');
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = '';
+        }
+    }
     
+   // Gets the entire gameboard 
+    const getBoard = () => board;
+    
+    // Prints the board object for now
+    const printBoard = () => console.log(board);
+
+    return {
+        getBoard,
+        printBoard
+    };
+
 })();
 
-const player = (name, mark) => {
-    return {name, mark};
-}
+const playerFactory = (name, mark) => {
+    return { name, mark };
+};
 
-const player1 = player('You', 'x');
-const player2 = player('Friend', 'o');
+const playerOne = playerFactory('Player X', 'X');
+const playerTwo = playerFactory('Player O', 'O');
 
-console.log(player1.name, player1.mark);
-console.log(player2.name, player2.mark);
+console.log(playerOne.name, playerOne.mark);
+console.log(playerTwo.name, playerTwo.mark);
+
+Gameboard.printBoard();
